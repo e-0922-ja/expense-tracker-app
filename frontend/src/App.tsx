e73f38f8b3cc3a5f1a0f53a177928c8fb15863f0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-08 23:25:16 +0900

    feat: add supabase base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./App.css";
+import { createClient } from "@supabase/supabase-js";
 
+console.log(process.env.REACT_APP_SUPABASE_URL);
+// Initialize the JS client
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL as string,
+  process.env.REACT_APP_SUPABASE_ANON_KEY as string
+);
+
+// Make a request
 function App() {
+  const test = async () => {
+    const { data, error } = await supabase.from("User").select("*");
+    console.log(data, error);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +30,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={test}>aaa</button>
       </header>
     </div>
   );
