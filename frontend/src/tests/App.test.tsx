7cdbfbe67d23ab90fb30cdfd4eda33efad9f29ac
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-17 21:01:39 +0900

    fix 🐛: add jest.config for github actions and add export to app.test

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -6,7 +6,7 @@
 //    sum(1, 2) => 3
 //  Example2:
 //    sum(5, 5) => 10
-export const sum = (a: number, b: number) => {
+const sum = (a: number, b: number) => {
   return a + b;
 };
 
@@ -25,3 +25,5 @@ test("adds 5 + 5 to equal 10", () => {
 test("adds 5 + 5 to not equal 3", () => {
   expect(sum(5, 5)).not.toBe(3);
 });
+
+export {};
