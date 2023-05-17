15d3e49891d66b58e4e11fd3900f138a277141cc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-17 22:09:23 +0900

    chore

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  // setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   verbose: true,
-  testMatch: ['<rootDir>/tests/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js|tsx)'],
+  testMatch: ['**/tests/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js|tsx)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
