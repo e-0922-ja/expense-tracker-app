1943cb7bf20035245e569a59c80888a9d6de4613
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-17 21:58:56 +0900

    fix🐛: change setuptest file path

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 module.exports = {
-  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   verbose: true,
-  testMatch: ['./src/tests/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js|tsx)'],
+  testMatch: ['<rootDir>/tests/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js|tsx)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
