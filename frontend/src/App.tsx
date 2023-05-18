8aa737dd3fce1d6a56f4483382e9357aff92d0bb
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-18 15:47:53 -0700

    docs: change header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./views/pages/HomePage";
 import { Header } from "./views/components/Header";
-import { Footer } from "./views/components/Footer";
 import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { selectTheme } from "./reducer/colorModeSlice";
 import { SignUpPage } from "./views/pages/SignUpPage";
 import { LoginPage } from "./views/pages/LoginPage";
 import { TransactionPage } from "./views/pages/TransactionPage";
+import { Toolbar } from "@mui/material";
 
 function App() {
   const theme = useSelector(selectTheme);
@@ -17,13 +17,13 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Header />
+        <Toolbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/transaction" element={<TransactionPage/>} />
+          <Route path="/transaction" element={<TransactionPage />} />
         </Routes>
-        <Footer />
       </ThemeProvider>
     </div>
   );
