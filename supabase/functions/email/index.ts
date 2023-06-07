298811d437fbcd656c3adcc54cfcc0cb50592da2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-07 15:57:26 +0900

    FIX🐛: To invoke from frontend, implemented CORS

--- supabase/functions/email/index.ts
+++ supabase/functions/email/index.ts
@@ -1,14 +1,24 @@
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type"
+}
+
 
 serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders })
+  }
+
   // https://supabase.com/docs/guides/functions/secrets
   const { toAddress, requestee } = await req.json();
 
-  const emailPattern = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
-  if (!emailPattern.test(toAddress)) {
-    return false
-  }
+  // This validation is not working in case of alias
+  // const emailPattern = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
+  // if (!emailPattern.test(toAddress)) {
+  //   return false
+  // }
 
   const apiKey = Deno.env.get('SENDGRID_API_KEY')
   const sendgridUri = "https://api.sendgrid.com/v3/mail/send"
@@ -27,5 +37,6 @@ serve(async (req) => {
     },
     method: "POST"
   })
+  console.log(response)
   return response
 });
