c34062cefe13a3260b5ae18431e355fc0682e39e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-21 22:33:01 +0900

    Fix lint

--- frontend/src/services/friendships/service.ts
+++ frontend/src/services/friendships/service.ts
@@ -2,18 +2,21 @@ import { client } from "../supabase/client";
 import { UpdateFriendShipRequest } from "./type";
 
 class _friendService {
-  updateFriendshipIdByFriendemail = async (
-    { friendId, friendEmail }: UpdateFriendShipRequest
-  ) => {
-    const { error } = await client.from('Friendships')
-      .update({ friendId }).eq('friendEmail', friendEmail)
+  updateFriendshipIdByFriendemail = async ({
+    friendId,
+    friendEmail,
+  }: UpdateFriendShipRequest) => {
+    const { error } = await client
+      .from("Friendships")
+      .update({ friendId })
+      .eq("friendEmail", friendEmail);
 
     if (error) {
       console.log(error);
       return { isError: true, message: error.message };
     }
     return { isError: false, message: "Success!" };
-  }
+  };
 }
 
 export const FriendshipService = new _friendService();
