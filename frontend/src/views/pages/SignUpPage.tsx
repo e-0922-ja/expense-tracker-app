fd713628701d4732503f3940b75bcbfa8b4c8bce
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-24 10:03:16 +0900

    FIX🐛: To suppresss build error, erase useState

--- frontend/src/views/pages/SignUpPage.tsx
+++ frontend/src/views/pages/SignUpPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 import { Button, InputAdornment, InputBase, Paper } from "@mui/material";
 import LockIcon from "@mui/icons-material/Lock";
