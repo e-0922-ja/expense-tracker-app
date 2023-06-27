8f2f7011e3c9d5f0019687c69cad0bc773ea9f01
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-27 21:10:44 +0900

    test

--- frontend/src/constants/routePaths.ts
+++ frontend/src/constants/routePaths.ts
@@ -1,55 +1,55 @@
 const home =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app"
     : "/";
 
 const signup =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/signup"
     : "/signup";
 
 const login =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/login"
     : "/login";
 
 const friendList =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/expenses/friendslist"
     : "/expenses/friendslist";
 
 const payment =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/payment"
     : "/expenses/payment";
 
 const history =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/history"
     : "/history";
 
 const historyDetail =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/history/:id"
     : "/history/:id";
 
 const historyGroup =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/history/group/"
     : "/history/group/";
 
 const account =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/account"
     : "/account";
 
 const friendApprove =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/friends"
     : "/friends";
 
 const passwordReset =
-  process.env.REACT_APP_ENVIRONMENT === "production"
+  process.env.REACT_APP_ENVIRONMENT === "roduction"
     ? "/expense-tracker-app/passwordReset"
     : "/passwordReset";
 
