66bd3bf55406ad75dbbc2844383c6cf0b1e69b20
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-27 20:53:03 +0900

    Replace to hashrouter on prd

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    {process.env.REACT_APP_ENVIRONMENT === "production" ? (
+      <HashRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </HashRouter>
+    ) : (
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    )}
   </React.StrictMode>
 );
