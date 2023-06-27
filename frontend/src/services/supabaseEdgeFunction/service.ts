1a634cc5e17245abbc85894de3e4df9bd16800c0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-27 20:53:31 +0900

    Implements apiKey to sendEmail

--- frontend/src/services/supabaseEdgeFunction/service.ts
+++ frontend/src/services/supabaseEdgeFunction/service.ts
@@ -1,9 +1,17 @@
+import { client } from "../supabase/client";
+
 class _supabaseEdgeFunctionService {
   private endpoint?: string;
+  private apiKey?: string;
 
   constructor() {
     if (!process.env.REACT_APP_SUPABASE_ENDPOINT)
       throw new Error("Missing SUPABASE_ENDPOINT env var");
+
+    (async () => {
+      const { data } = await client.auth.getSession();
+      this.apiKey = data.session?.access_token;
+    })();
 
     this.endpoint = process.env.REACT_APP_SUPABASE_ENDPOINT;
   }
@@ -13,8 +21,7 @@ class _supabaseEdgeFunctionService {
     const response = await fetch(`${this.endpoint}/email`, {
       body,
       headers: {
-        // Will implements JWT auth later
-        // Authorization: `Bearer ${apiKey}`,
+        Authorization: `Bearer ${this.apiKey}`,
         "Content-Type": "application/json",
       },
       method: "POST",
