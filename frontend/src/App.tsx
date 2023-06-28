9805895b75fdd0607157ea387b9cd791543350d9
yuki-o1o5 <jkksst1712@gmail.com>
2023-06-27 17:39:08 -0700

    docs: change path name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -50,7 +50,7 @@ function App() {
             element={<ProtectedRoute element={<FriendsHistoryDetailPage />} />}
           />
           <Route
-            path="/friends"
+            path="/friendsList"
             element={<ProtectedRoute element={<FriendsListPage />} />}
           />
           <Route
